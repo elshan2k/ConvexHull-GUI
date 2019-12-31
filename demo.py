c930a4dec7e1ba05cbaeaf330af159016f311544
philippeitis <33013301+philippeitis@users.noreply.github.com>
2019-12-31 10:01:43 -0800

    Reduce run time by 1/2, minor tweaks
    I removed unused imports, added an if __name__ == "__main__", and made a few tweaks to the convexHull function to make it faster and added line removal, so the points are kept but the lines are erased.
    I also made points tuples.

--- demo.py
+++ demo.py
@@ -1,74 +1,71 @@
-from tkinter import Tk, Canvas, Frame, BOTH, Button,Frame
-import math
-array = []
+from tkinter import Canvas, BOTH, Button, Frame
+
+points = []
+lines = []
 canvas = None
 frame = None
-res = None
+
+
 def on_dot_click(event):
     x = event.x
     y = event.y
-    array.append([x, y])
-    canvas.create_rectangle(x, y, x+3, y+3, fill="#476042")
+    points.append((x, y))
+    canvas.create_rectangle(x, y, x + 3, y + 3, fill="#476042")
 
 
-def resetCanvas(e):
+def reset_canvas(e):
     canvas.delete("all")
-    global array
-    array = []
-    res = None
-    print('reset')
+    points.clear()
+    lines.clear()
+    print("Reset")
 
 
-def convexHull(ar): 
-    ar = array 
-    n = len(ar)
-    global res
-    res = []
-    for i in range(0, n):
-        gre = 0
-        less = 0
-        for j in range(0, n):
-            if i != j:
-                a = ar[j][1]-ar[i][1]
-                b = ar[i][0]-ar[j][0]
-                c = a*(ar[i][0]) + b*(ar[i][1])
-                for k in range(n):
-                    check =a*ar[k][0] + b*ar[k][1]
+def convex_hull(e):
+    for line in lines:
+        canvas.delete(line)
 
-                    if check > c:
-                        gre +=1
-                    elif check < c:
-                        less += 1   
-                if (gre == 0 and less !=0) or (less ==0 and gre != 0):
-                    gre =0
-                    less =0
-                    canvas.create_line(ar[i][0], ar[i][1], ar[j][0], ar[j][1])
-                    if (ar[i] not in res):
-                        res.append(ar[i])
-                        
-                else:
-                    gre =0
-                    less =0
-    if (len(res) ==0):
+    # res = []
+
+    for i, point_i in enumerate(points):
+        for point_j in points[i:]:
+            greater = False
+            less = False
+
+            a = point_j[1] - point_i[1]
+            b = point_i[0] - point_j[0]
+            c = a * (point_i[0]) + b * (point_i[1])
+
+            for point in points:
+                check = a * point[0] + b * point[1]
+
+                if check > c:
+                    greater = True
+                elif check < c:
+                    less = True
+
+            if (not greater and less) or (greater and not less):
+                lines.append(canvas.create_line(point_i[0], point_i[1], point_j[0], point_j[1]))
+                # if point_i not in res:
+                #     res.append(point_i)
+
+    if not len(lines):
         return "Impossible"
-    res = []
+    # return res
 
 
-frame = Frame(None, bg='grey', height=2)
-frame.pack()
+if __name__ == "__main__":
+    frame = Frame(None, bg='grey', height=2)
+    frame.pack()
 
-canvas = Canvas(frame, width="600", height="600")
-canvas.pack(fill=BOTH, expand=1)
-button = Button(frame, text="Start")
-button.bind('<Button-1>', convexHull)
-button.pack(side='left', padx=10)
-reset = Button(frame, text="Reset")
-reset.pack(side='left', padx=10)
-reset.bind('<Button-1>', resetCanvas)
+    canvas = Canvas(frame, width="600", height="600")
+    canvas.pack(fill=BOTH, expand=1)
+    button = Button(frame, text="Start")
+    button.bind('<Button-1>', convex_hull)
+    button.pack(side='left', padx=10)
+    reset = Button(frame, text="Reset")
+    reset.pack(side='left', padx=10)
+    reset.bind('<Button-1>', reset_canvas)
 
-canvas.bind('<Button-1>', on_dot_click)
-frame.mainloop()
-#print(array)
-
-
-
+    canvas.bind('<Button-1>', on_dot_click)
+    frame.mainloop()
+    # print(points)
